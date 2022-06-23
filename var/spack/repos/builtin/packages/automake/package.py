1c15b58201aaf3877a1f1eddfb2acd3da85376ec
Tre' Jeter <tjeter97@gmail.com>
2022-06-22 20:25:08 -0400

    Updated for CVE exploration

--- var/spack/repos/builtin/packages/automake/package.py
+++ var/spack/repos/builtin/packages/automake/package.py
@@ -23,6 +23,18 @@ class Automake(AutotoolsPackage, GNUMirrorPackage):
     version('1.13.4', sha256='4c93abc0bff54b296f41f92dd3aa1e73e554265a6f719df465574983ef6f878c')
     version('1.11.6', sha256='53dbf1945401c43f4ce19c1971baecdbf8bc32e0f37fa3f49fe7b6992d0d2030')
 
+    cpe = {
+            '1.16.5':'cpe:2.3:a:gnu:automake:1.16.5:*:*:*:*:*:*:*',
+            '1.16.3':'cpe:2.3:a:gnu:automake:1.16.3:*:*:*:*:*:*:*',
+            '1.16.2':'cpe:2.3:a:gnu:automake:1.16.2:*:*:*:*:*:*:*',
+            '1.16.1':'cpe:2.3:a:gnu:automake:1.16.1:*:*:*:*:*:*:*',
+            '1.15.1':'cpe:2.3:a:gnu:automake:1.15.1:*:*:*:*:*:*:*',
+            '1.15':'cpe:2.3:a:gnu:automake:1.15:*:*:*:*:*:*:*',
+            '1.14.1':'cpe:2.3:a:gnu:automake:1.14.1:*:*:*:*:*:*:*',
+            '1.13.4':'cpe:2.3:a:gnu:automake:1.13.4:*:*:*:*:*:*:*',
+            '1.11.6':'cpe:2.3:a:gnu:automake:1.11.6:*:*:*:*:*:*:*'
+          }
+
     depends_on('autoconf', type='build')
     depends_on('perl', type=('build', 'run'))
 
