818c95e12f2e3271a21e5e2e50cdb59f9a747255
matt bobbitt <mattbobbitt3@gmail.com>
2022-07-07 13:53:29 -0700

    added bogus cpe dict for http-parser

--- var/spack/repos/builtin/packages/http-parser/package.py
+++ var/spack/repos/builtin/packages/http-parser/package.py
@@ -16,6 +16,11 @@ class HttpParser(MakefilePackage):
     version('2.9.3', sha256='8fa0ab8770fd8425a9b431fdbf91623c4d7a9cdb842b9339289bd2b0b01b0d3d')
     version('2.9.2', sha256='5199500e352584852c95c13423edc5f0cb329297c81dd69c3c8f52a75496da08')
     version('2.9.1', sha256='33220771208bcacecd970b6de03bebe239374a8e9cf3baeda79b4f3920bede21')
-
+    cpe = {
+            '2.9.4':'cpe:2.3:a:nodejs:http-parser:2.9.4',
+            '2.9.3':'cpe:2.3:a:nodejs:http-parser:2.9.3',
+            '2.9.2':'cpe:2.3:a:nodejs:http-parser:2.9.2',
+            '2.9.1':'cpe:2.3:a:nodejs:http-parser:2.9.1',
+    }
     def install(self, spec, prefix):
         make('install', "DESTDIR=%s" % prefix, "PREFIX=")
